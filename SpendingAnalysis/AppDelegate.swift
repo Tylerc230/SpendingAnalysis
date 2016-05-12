28b0f85e75cc366f56b33d63cccd1953065c6f27
Tyler Casselman <tylerc230@gmail.com>
2016-05-11 17:24:48 -0700

    Initial Commit

---
+++ SpendingAnalysis/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  SpendingAnalysis
+//
+//  Created by Tyler Casselman on 5/11/16.
+//  Copyright © 2016 13bit consulting. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
